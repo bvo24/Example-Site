0fe89512eded9e3ce0ac2f135ad217dc108fa485
bvo24 <130068413+bvo24@users.noreply.github.com>
2024-11-04 21:11:40 -0500

    Visual Update

--- script.js
+++
@@ -1,7 +0,0 @@
-// script.js
-
-// Function to toggle dropdown sections
-function toggleSection(sectionId) {
-    const section = document.getElementById(sectionId);
-    section.style.display = section.style.display === 'block' ? 'none' : 'block';
-}
